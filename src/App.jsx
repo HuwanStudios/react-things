62bca19c3da727fbaadfb14d058f705b598f76ee
Steven <Huwan.Studios@gmail.com>
2024-03-01 11:58:16 -0500

    Passing props to a component - extract a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import Gallery from './Gallery';
-import GalleryTwo from './GalleryTwo';
 // import GalleryTwo from './GalleryTwo.jsx'; // Adjust the import path with .jsx extension
 // import getImageUrl from './Utils';
 // import Bio from './Bio';
 // import Congratulations from './Congratulations';
 // import { Profile, ProfileTwo, ProfileThree } from './Profile'
 // import {TodoList, TodoListTwo} from './TodoList';
+
+import GalleryTwo from "./GalleryTwo";
 
 
 
