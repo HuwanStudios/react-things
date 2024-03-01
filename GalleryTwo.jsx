d3f2ea3930f054b1821e2f0ed81317859cd1e517
Steven <Huwan.Studios@gmail.com>
2024-03-01 04:45:03 -0500

    06-extract-a-component

---
+++ GalleryTwo.jsx
@@ -0,0 +1,68 @@
+import { getImageUrl } from "./Utils";
+
+// function Avatar ({person, size = 70}){
+//     // person and size available 
+//     return(
+//         <img
+//             className="avatar"
+//             src={getImageUrl(person.imageId)}
+//             alt={person.name}
+//             width={size}
+//             height={size}
+//         />
+//         );
+// }
+
+
+function Profile({person, size = 70, profession, awards, discovered}) {
+    const imageUrl = getImageUrl(person); //Get the imaage URL for the person
+
+    return (
+      <>
+          <img
+             className="avatar"
+             src={getImageUrl}
+             alt={person.name}
+             width={size}
+             height={size}
+         />
+            <h2>{person.name}</h2>
+            <p><b>Profession:</b>{profession}</p>
+            <p><b>Awards:</b>{awards}</p>
+            <p><b>Discovered:</b>{discovered}</p>
+        </>
+    );
+  }
+
+  function GalleryTwo(){
+    const size = 70; //Default size for avatars
+
+    return(
+    <>
+        <div>
+            <Profile
+                size={size} // Pass in size or 70
+                person={{ 
+                    name: 'Maria Skłodowska-Curie', 
+                    imageId: 'szV5sdG',
+                }}
+                profession="Physicist and Chemist"
+                awards="Nobel Prize in Physics, Nobel Prize in Chemistry"
+                discovered="Polonium, Radium"
+            />
+            <Profile
+                size={size}
+                person={{
+                    name: 'Katsuko Saruhashi', 
+                    imageId: 'YfeOqp2',
+                }}
+                profession="Geochemist"
+                awards="Miyake Prize for Geochemistry"
+                discovered="Methodologies in measuring carbon dioxide in seawater"
+            />
+        </div>
+    </>
+    );
+  }
+
+export default GalleryTwo;
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import Gallery from '../Gallery';
-import Bio from './Bio';
-import Congratulations from './Congratulations';
-import { Profile, ProfileTwo } from './Profile'
-import {TodoList, TodoListTwo} from './TodoList';
+// import Gallery from './Gallery';
+import GalleryTwo from './GalleryTwo';
+// import GalleryTwo from './GalleryTwo.jsx'; // Adjust the import path with .jsx extension
+// import getImageUrl from './Utils';
+// import Bio from './Bio';
+// import Congratulations from './Congratulations';
+// import { Profile, ProfileTwo, ProfileThree } from './Profile'
+// import {TodoList, TodoListTwo} from './TodoList';
+
 
 
 function App() {
   
   return (
     <> 
-    <Profile/ >
+    {/* <ProfileThree/ >
     <ProfileTwo/ >
     <Gallery />
     <Congratulations />
     <TodoList />
     <Bio />
-    <TodoListTwo/>
+    <TodoListTwo />
+   */}
+    <getImageUrl />
+    <GalleryTwo />
     </>
-  )
+  );
 }
 
 export default App;
