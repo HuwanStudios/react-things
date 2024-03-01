a1636d475c9cfba8dcd0ab7cbc585d4bca4ff021
Steven <Huwan.Studios@gmail.com>
2024-02-29 21:28:39 -0500

    writing markup with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import Gallery from '../Gallery';
+import Bio from './Bio';
 import Congratulations from './Congratulations';
 import { Profile, ProfileTwo } from './Profile'
+import TodoList from './TodoList';
 
 
 function App() {
-
+  
   return (
     <> 
     <Profile/ >
     <ProfileTwo/ >
     <Gallery />
     <Congratulations />
+    <TodoList />
+    <Bio />
     </>
   )
 }
 
 export default App;
+
