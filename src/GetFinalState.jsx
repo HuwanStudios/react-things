238081c2bee9dc8e6fb2cc52f0b52527a138a241
Steven Julien-Stewart <Huwan.Studios@gmail.com>
2024-03-13 05:06:28 -0400

    //14-queing-a-series-of-state-updates - Challenge 2 of 2: Implement the state queue yourself

---
+++ src/GetFinalState.jsx
@@ -0,0 +1,46 @@
+export function getFinalState(baseState, queue) {
+    // const [baseState, setBaseState] = useState(0);
+    // const
+    let finalState = baseState;
+
+    // TODO: do something with the queue...
+
+    return finalState(
+    // return
+    //   <>
+    //     <button onClick={() => {
+    //       setNumber(1);
+    //       setNumber(1);
+    //       setNumber(1);
+    //     }}>+3</button>
+    //   </>
+    //   <>
+    //   <button onClick={() => {
+    //     setNumber(n => n + 1);
+    //     setNumber(n => n + 1);
+    //   }}>+3</button>
+    // </>
+    //   <>
+    //   <button onClick={() => {
+    //     setNumber(n => n + 1);
+    //     setNumber(n => n + 1);
+    //     setNumber(n => n + 1);
+    //   }}>+3</button>
+    // </>
+    //   <>
+    //   <button onClick={() => {
+    //     setNumber(number + 5);
+    //     setNumber(n => n + 1);
+    //   }}>Increase the number</button>
+    // </>
+    // <>
+    //   <button onClick={() => {
+    //   setNumber(number + 5);
+    //   setNumber(n => n + 1);
+    //   setNumber(42);
+    // }}>
+    // </>
+    );
+}
+
+//14-Queuing a series of state updates - Challenge 2 of 2: Implement the state queue yourself
