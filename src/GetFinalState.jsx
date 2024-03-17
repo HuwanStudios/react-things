a3b0e9438a3578b3f5d5c979869381924dbd245a
Steven Julien-Stewart <Huwan.Studios@gmail.com>
2024-03-17 17:49:51 -0400

    // 15-updating-objects-in-state - Challenge 2 of 2: Find and fix the mutation

--- src/GetFinalState.jsx
+++ src/GetFinalState.jsx
@@ -1,46 +1,74 @@
-export function getFinalState(baseState, queue) {
-    // const [baseState, setBaseState] = useState(0);
-    // const
-    let finalState = baseState;
+// export function getFinalState(baseState, queue) {
+//     // const [baseState, setBaseState] = useState(0);
+//     // const
+//     let finalState = baseState;
+//
+//     // TODO: do something with the queue...
+//
+//     return finalState(
+//       <>
+//         <button onClick={() => {
+//           setNumber(1);
+//           setNumber(1);
+//           setNumber(1);
+//         }}>+3</button>
+//       </>
+//       <>
+//       <button onClick={() => {
+//         setNumber(n => n + 1);
+//         setNumber(n => n + 1);
+//       }}>+3</button>
+//     </>
+//       <>
+//       <button onClick={() => {
+//         setNumber(n => n + 1);
+//         setNumber(n => n + 1);
+//         setNumber(n => n + 1);
+//       }}>+3</button>
+//     </>
+//       <>
+//       <button onClick={() => {
+//         setNumber(number + 5);
+//         setNumber(n => n + 1);
+//       }}>Increase the number</button>
+//     </>
+//     <>
+//       <button onClick={() => {
+//       setNumber(number + 5);
+//       setNumber(n => n + 1);
+//       setNumber(42);
+//     }}>
+//     </>
+//     );
+// }
 
-    // TODO: do something with the queue...
+export function getFinalState(baseState, queues) {
+    return queues.map(queue => {
+        let finalState = baseState;
 
-    return finalState(
-    // return
-    //   <>
-    //     <button onClick={() => {
-    //       setNumber(1);
-    //       setNumber(1);
-    //       setNumber(1);
-    //     }}>+3</button>
-    //   </>
-    //   <>
-    //   <button onClick={() => {
-    //     setNumber(n => n + 1);
-    //     setNumber(n => n + 1);
-    //   }}>+3</button>
-    // </>
-    //   <>
-    //   <button onClick={() => {
-    //     setNumber(n => n + 1);
-    //     setNumber(n => n + 1);
-    //     setNumber(n => n + 1);
-    //   }}>+3</button>
-    // </>
-    //   <>
-    //   <button onClick={() => {
-    //     setNumber(number + 5);
-    //     setNumber(n => n + 1);
-    //   }}>Increase the number</button>
-    // </>
-    // <>
-    //   <button onClick={() => {
-    //   setNumber(number + 5);
-    //   setNumber(n => n + 1);
-    //   setNumber(42);
-    // }}>
-    // </>
-    );
+        // Apply each action in the queue to the baseState
+        queue.map(action => {
+            finalState = typeof action === 'function' ? action(finalState) : finalState + action;
+        });
+
+        // Expected result for the current test case
+        const expectedResult = finalState;
+
+        return { finalState, expectedResult };
+    });
 }
+
+// Test cases
+const baseState = 0;
+const queues = [
+    [1, 1, 1],
+    [n => n+1, n => n+1, n => n+1],
+    [5, n => n+1],
+    [5, n => n+1, 42]
+];
+
+// Run the tests
+const testResults = getFinalState(baseState, queues);
+console.log(testResults);
 
 //15-Queuing a series of state updates - Challenge 2 of 2: Implement the state queue yourself
